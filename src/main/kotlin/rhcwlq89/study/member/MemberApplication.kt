aa685de2a6dd68bee999914bf6d3cd1032f34da5
이호종 <ccaja21@naver.com>
2024-03-19 01:29:38 +0900

    git init

---
+++ src/main/kotlin/rhcwlq89/study/member/MemberApplication.kt
@@ -0,0 +1,11 @@
+package rhcwlq89.study.member
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MemberApplication
+
+fun main(args: Array<String>) {
+	runApplication<MemberApplication>(*args)
+}
---
+++ src/test/kotlin/rhcwlq89/study/member/MemberApplicationTests.kt
@@ -0,0 +1,13 @@
+package rhcwlq89.study.member
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
